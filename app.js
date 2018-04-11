d9c4345befb6e0255dc7b2e63967e87bae2f9a30
Gregor Laan <gregor@github.ee>
2018-04-11 22:43:56 +0300

    Add some demo data and display them

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 var MDK = angular.module('MDK', []);
 
 MDK.controller('DevicesListController', function DevicesListController($scope) {
-    
+
+    $scope.devices = [
+        {
+            DeviceName: "Galaxy S7 Edge",
+            Brand: "Samsung"
+        },
+        {
+            DeviceName: "3310",
+            Brand: "Nokia"
+        },
+        {
+            DeviceName: "P9 lite mini",
+            Brand: "Huawei"
+        }
+    ]
+
 });
