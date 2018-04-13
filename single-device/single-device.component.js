4431d44e981a85aa67309d9216a2a78987187b36
Gregor Laan <gregor@github.ee>
2018-04-13 23:57:56 +0300

    Get localStorage item named device
    Also for testing display DeviceName.

--- single-device/single-device.component.js
+++ single-device/single-device.component.js
@@ -2,9 +2,16 @@ angular.
   module('singleDevice').
   component('singleDevice', {
     templateUrl: 'single-device/single-device.template.html',
-    controller: ['$routeParams',
-      function SingleDeviceController($routeParams) {
-        this.deviceId = $routeParams.deviceId;
-      } 
+    controller: ['$scope',
+      function SingleDeviceController($scope) {
+        var self = this;
+        self.device = null;
+
+        Storage.prototype.getObject = function(key) {
+            var value = this.getItem(key);
+            return value && JSON.parse(value);
+        }
+        $scope.device = localStorage.getObject('device');
+        } 
     ]
   });
