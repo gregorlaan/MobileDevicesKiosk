a5af73fa62c2badf5e83594425f7ebedcb08d74b
Gregor Laan <gregor@github.ee>
2018-04-13 23:36:11 +0300

    Set localStorage item named device

--- devices-list/devices-list.component.js
+++ devices-list/devices-list.component.js
@@ -2,13 +2,24 @@ angular.
   module('devicesList').
   component('devicesList', {
     templateUrl: 'devices-list/devices-list.template.html',
-    controller: [ '$http',
-      function DevicesListController($http) {
+    controller: [ '$http','$scope',
+      function DevicesListController($http, $scope) {
         var self = this;
 
         $http.get('devices/devices.json').then(function(response) {
           self.devices = response.data
         });
+
+        $scope.selectDevice = function(device) {
+          Storage.prototype.setObject = function(key, value) {
+            this.setItem(key, JSON.stringify(value));
+          }
+          Storage.prototype.getObject = function(key) {
+              var value = this.getItem(key);
+              return value && JSON.parse(value);
+          }
+          localStorage.setObject('device', device);
+        }
       }
     ]
   });
