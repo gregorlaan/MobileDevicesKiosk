e76f80fd9fd705eb801b26aa7f491de4252fd5e6
Gregor Laan <gregor@github.ee>
2018-04-13 12:44:22 +0300

    Get devices over HTTP recuest

--- devices-list/devices-list.component.js
+++ devices-list/devices-list.component.js
@@ -2,20 +2,11 @@ angular.
   module('devicesList').
   component('devicesList', {
     templateUrl: 'devices-list/devices-list.template.html',
-    controller: function DevicesListController() {
-      this.devices = [
-        {
-          DeviceName: "Galaxy S7 Edge",
-          Brand: "Samsung"
-        },
-        {
-          DeviceName: "3310",
-          Brand: "Nokia"
-        },
-        {
-          DeviceName: "P9 lite mini",
-          Brand: "Huawei"
-        }
-      ];
+    controller: function DevicesListController($http) {
+      var self = this;
+
+      $http.get('devices/devices.json').then(function(response) {
+        self.devices = response.data
+      });
     }
   });
