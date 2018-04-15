bfdf1b1fc6aede9a1efc2af6b8fbebf306122f4e
Gregor Laan <gregor@github.ee>
2018-04-15 22:19:05 +0300

    Add toggle option for hiding tables

--- single-device/single-device.component.js
+++ single-device/single-device.component.js
@@ -7,11 +7,18 @@ angular.
         var self = this;
         self.device = null;
 
+        $scope.showGeneralInformation = true;
+        $scope.showHardware = true;
+        $scope.showCamera = true;
+        $scope.showFeatures = true;
+        $scope.showDimensions = true;
+        $scope.showConnectivity = true;
+
         Storage.prototype.getObject = function(key) {
             var value = this.getItem(key);
             return value && JSON.parse(value);
         }
         $scope.device = localStorage.getObject('device');
-        } 
+      }
     ]
   });
