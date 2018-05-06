cb789ad2978fdd9f6612955994412bb519e0a4d5
Gregor Laan <gregor@github.ee>
2018-05-06 14:49:43 +0300

    Display alert when device added to the kiosk

--- devices-list/devices-list.component.js
+++ devices-list/devices-list.component.js
@@ -2,21 +2,23 @@ angular.
   module('devicesList').
   component('devicesList', {
     templateUrl: 'devices-list/devices-list.template.html',
-    controller: [ '$http','$scope',
+    controller: ['$http', '$scope',
       function DevicesListController($http, $scope) {
         var self = this;
+        $scope.alert = false;
 
-        $http.get('devices/devices.json').then(function(response) {
+        $http.get('devices/devices.json').then(function (response) {
           self.devices = response.data
         });
 
-        $scope.selectDevice = function(device) {
-          Storage.prototype.setObject = function(key, value) {
+        $scope.selectDevice = function (device) {
+          $scope.alert = device.DeviceName;
+          Storage.prototype.setObject = function (key, value) {
             this.setItem(key, JSON.stringify(value));
           }
-          Storage.prototype.getObject = function(key) {
-              var value = this.getItem(key);
-              return value && JSON.parse(value);
+          Storage.prototype.getObject = function (key) {
+            var value = this.getItem(key);
+            return value && JSON.parse(value);
           }
           localStorage.setObject('device', device);
         }
